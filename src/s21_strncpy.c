9937625c5b0b207a9cbad112af2ceea95d9ac9d0
Flute <flatmind13@mail.ru>
2025-01-07 20:51:04 +0300

    final version strncpy

--- src/s21_strncpy.c
+++ src/s21_strncpy.c
@@ -2,17 +2,21 @@
 
 char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
   s21_size_t dest_length = 0;
+  s21_size_t src_length = 0;
+  s21_size_t count = 0;
   s21_size_t index = 0;
 
-  dest_length = s21_strlen(dest);
+  count = n;
 
-  for (; index < n; dest[index] = src[index], index++)
+  dest_length = s21_strlen(dest);
+  src_length = s21_strlen(src);
+
+  for (; index < count; dest[index] = src[index], index++)
     ;
 
-  //по справочнику этого быть не должно,а оно есть. Возможно какая то плюсовая
-  //фигня
-  // if(index<dest_length)
-  //     for(;index<dest_length;dest[index]='\0',index++);
+  if (count >= src_length)
+    for (; index < dest_length; dest[index] = '\0', index++)
+      ;
 
   return dest;
 }
