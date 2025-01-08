e898d1a143f6bf8f0d4038bd4cc0a3cb466e064d
Flute <flatmind13@mail.ru>
2025-01-08 18:52:21 +0300

    оптимизировал и добавил тестов к strcspn

--- tests/s21_strcspn_test.c
+++ tests/s21_strcspn_test.c
@@ -68,6 +68,57 @@ START_TEST(test_s21_strcspn_4) {
 }
 END_TEST
 
+//исходная строка
+// char *str = "there is no power as no knowledge";
+//пустая строка символов
+// char *tk = ""
+START_TEST(test_s21_strcspn_5) {
+  char str[] = "there is no power as no knowledge";
+  char *tk = "";
+  size_t s21_strcspn_result = 0;
+  size_t strcspn_result = 0;
+
+  s21_strcspn_result = s21_strcspn(str, tk);
+  strcspn_result = strcspn(str, tk);
+
+  ck_assert(s21_strcspn_result == strcspn_result);
+}
+END_TEST
+
+//пустая исходная строка
+// char *str = "";
+//строка для поиска символов с существующим символом внутри строки
+// char *tk = ",.t!?o"
+START_TEST(test_s21_strcspn_6) {
+  char str[] = "";
+  char *tk = ",.t!?o";
+  size_t s21_strcspn_result = 0;
+  size_t strcspn_result = 0;
+
+  s21_strcspn_result = s21_strcspn(str, tk);
+  strcspn_result = strcspn(str, tk);
+
+  ck_assert(s21_strcspn_result == strcspn_result);
+}
+END_TEST
+
+//пустая исходная строка
+// char *str = "";
+//пустая строка символов
+// char *tk = ""
+START_TEST(test_s21_strcspn_7) {
+  char str[] = "";
+  char *tk = "";
+  size_t s21_strcspn_result = 0;
+  size_t strcspn_result = 0;
+
+  s21_strcspn_result = s21_strcspn(str, tk);
+  strcspn_result = strcspn(str, tk);
+
+  ck_assert(s21_strcspn_result == strcspn_result);
+}
+END_TEST
+
 // const string or null ptr => sega
 Suite *s21_strcspn_suite(void) {
   Suite *suite;
@@ -103,6 +154,27 @@ Suite *s21_strcspn_suite(void) {
   //строка для поиска символов с существующим символом внутри строки
   // char *tk = ",.t!?o"
   tcase_add_test(tc_s21_strcspn, test_s21_strcspn_4);
+  suite_add_tcase(suite, tc_s21_strcspn);
+
+  //исходная строка
+  // char *str = "there is no power as no knowledge";
+  //пустая строка символов
+  // char *tk = ""
+  tcase_add_test(tc_s21_strcspn, test_s21_strcspn_5);
+  suite_add_tcase(suite, tc_s21_strcspn);
+
+  //пустая исходная строка
+  // char *str = "";
+  //строка для поиска символов с существующим символом внутри строки
+  // char *tk = ",.t!?o"
+  tcase_add_test(tc_s21_strcspn, test_s21_strcspn_6);
+  suite_add_tcase(suite, tc_s21_strcspn);
+
+  //пустая исходная строка
+  // char *str = "";
+  //пустая строка символов
+  // char *tk = ""
+  tcase_add_test(tc_s21_strcspn, test_s21_strcspn_7);
   suite_add_tcase(suite, tc_s21_strcspn);
 
   return suite;
