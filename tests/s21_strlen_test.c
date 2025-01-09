9c25c2f0c572a0c6ec7a6c71ead581bfcf92c5e7
Flute <flatmind13@mail.ru>
2025-01-09 14:03:10 +0300

    тесты strlen отрефакторены

--- tests/s21_strlen_test.c
+++ tests/s21_strlen_test.c
@@ -1,8 +1,8 @@
 #include "s21_check_string.h"
 
-// same string
+//  char* str = "there is no power as no knowledge";
 START_TEST(test_s21_strlen_1) {
-  char *str = T21_STR_1;
+  char *str = "there is no power as no knowledge";
   s21_size_t s21_str_size = 0;
   size_t str_size = 0;
 
@@ -10,36 +10,35 @@ START_TEST(test_s21_strlen_1) {
   str_size = strlen(str);
 
   ck_assert(s21_str_size == str_size);
-  // or
-
-  // ck_assert_msg(s21_str_size==str_size,"Значения размеров строки:
-  // %s\nсовпадают", str);
 }
 END_TEST
 
-//разные строки
+//  char* str_s21 = "there is no power as no knowledge";
+//  char* str = "Hello world";
 START_TEST(test_s21_strlen_2) {
-  char *str = T21_STR_1;
+  char *str_s21 = "there is no power as no knowledge";
+  char *str = "Hello world";
   s21_size_t s21_str_size = 0;
   size_t str_size = 0;
 
-  s21_str_size = s21_strlen(T_STR_SHORT);
+  s21_str_size = s21_strlen(str_s21);
   str_size = strlen(str);
   // ck_assert(s21_str_size==str_size);
   // or
 
-  ck_assert_msg(s21_str_size < str_size,
+  ck_assert_msg(s21_str_size > str_size,
                 "Значения размеров строки: %s\nсовпадают", str);
 }
 END_TEST
 
-//пустая строка
+//  char* str = "";
 START_TEST(test_s21_strlen_3) {
+  char *str = "";
   s21_size_t s21_str_size = 0;
   size_t str_size = 0;
 
-  s21_str_size = s21_strlen(T_EMPTY);
-  str_size = strlen(T_EMPTY);
+  s21_str_size = s21_strlen(str);
+  str_size = strlen(str);
 
   ck_assert_msg(s21_str_size == str_size,
                 "Значения размеров строки: %ld и %ld не совпадают", str_size,
@@ -56,12 +55,16 @@ Suite *s21_strlen_suite(void) {
 
   tc_s21_strlen = tcase_create("S21_strlen");
 
+  //  char* str = "there is no power as no knowledge";
   tcase_add_test(tc_s21_strlen, test_s21_strlen_1);
   suite_add_tcase(suite, tc_s21_strlen);
 
+  //  char* str_s21 = "there is no power as no knowledge";
+  //  char* str = "Hello world";
   tcase_add_test(tc_s21_strlen, test_s21_strlen_2);
   suite_add_tcase(suite, tc_s21_strlen);
 
+  //  char* str = "";
   tcase_add_test(tc_s21_strlen, test_s21_strlen_3);
   suite_add_tcase(suite, tc_s21_strlen);
 
